d99abb708b228b29aeaeae3af90dd7fa943b4224
Mike Kipp <168943773+shadowlord1012@users.noreply.github.com>
2024-12-01 21:47:44 -0500

    Add files via upload

--- Final Project/js/JSForPages.js
+++ Final Project/js/JSForPages.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(function() {
         $("#head").load("metadata.html");
-        $("#header").load("header.html");
+        $("header").load("header.html");
         $("#footer").load("footer.html");
     })
 })
